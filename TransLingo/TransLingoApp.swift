61f5978dbc61bb47f1b06fc350260420f97cd653
LiamHsu <s459958@gmail.com>
2024-08-14 14:45:48 +0800

    Init Xcode project

---
+++ TransLingo/TransLingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransLingoApp.swift
+//  TransLingo
+//
+//  Created by Liam on 2024/8/14.
+//
+
+import SwiftUI
+
+@main
+struct TransLingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
